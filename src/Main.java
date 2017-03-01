99f070094dab7f9dc5667f630a084d5d6a731210
Spriithy <theo.dano@gmail.com>
2017-03-01 21:04:30 +0100

    Runtime Lib rebase

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,8 @@
 
-import org.lightscript.rt.LSNumber;
-import org.lightscript.rt.LSString;
-import org.lightscript.rt.LSVector;
-
 public class Main {
 
 	public static void main(String[] args) {
-		// Vectors tests
-		
-		LSNumber x = new LSNumber("1.1783e+3");
-		System.out.println(x.hashString());
-		
-		LSVector vector = new LSVector(x.type(), 5);
-		vector.set(3, x);
-		System.out.println(vector);
-		LSVector vector2 = new LSVector(vector.type(), 5);
-		vector2.appendCopy(vector);
-		vector.set(0, x);
-		System.out.println(vector2);
-		
-		System.out.println();
-		LSVector vector3 = (LSVector) vector2.get(5);
-		vector3.set(0, x.toInteger().toDecimal());
-		System.out.println(vector);
-		System.out.println(vector2.get(5));
-		
-		// String tests
-		
-		LSString str = new LSString("Hey\nthere !");
-		System.out.println(str.capacity());
-		System.out.println(str.get(3));
-		str.set(3, new LSNumber((long) 0x1b));
-		System.out.println(str.printString());
+
 	}
 
 }
--- src/org/lightscript/rt/TypeMismatchException.java
+++ src/org/lightscript/rt/TypeMismatchException.java
@@ -3,23 +3,8 @@ package org.lightscript.rt;
 public class TypeMismatchException extends Error {
 	private static final long serialVersionUID = -2375950705359478796L;
 
-	public TypeMismatchException() {}
-
-	public TypeMismatchException(String message) {
-		super(message);
-	}
-
-	public TypeMismatchException(Throwable cause) {
-		super(cause);
-	}
-
-	public TypeMismatchException(String message, Throwable cause) {
-		super(message, cause);
-	}
-
-	public TypeMismatchException(String message, Throwable cause, boolean enableSuppression,
-			boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
+	public TypeMismatchException(String t1, String t2) {
+		super(t2 + " does not match expected type " + t1);
 	}
 
 }
